0d626161f0336d1d261addc82b2d24c37bcd5944
TSunDanceK <sonnybrindle@hotmail.com>
2026-03-06 14:16:37 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "mystockharbour.com",
+          },
+        ],
+        destination: "https://mystockharbor.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.mystockharbour.com",
+          },
+        ],
+        destination: "https://mystockharbor.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
